289949cdb71a9407ae5ef7090bb06f1f4d0fb046
Stephen Fox <stevethecoder34@gmail.com>
2015-10-27 10:11:37 -0600

    Extracted an interface

--- src/copsandrobbers/RandomNumberGenerator.java
+++ src/copsandrobbers/RandomNumberGenerator.java
@@ -1,26 +1,29 @@
 package copsandrobbers;
 
+import copsandrobbers.randomgeneration.RandomCoordinateGenerator;
+
 /**
  * Created by jjensen on 10/26/15.
  */
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements RandomCoordinateGenerator {
 
     private int min;
     private int max;
     private int currentNumber = 0;
 
-    public RandomNumberGenerator(int min, int max) {
+    public RandomNumberGenerator( int min, int max ) {
         this.min = min;
         this.max = max;
         currentNumber = min;
     }
 
+    @Override
     public Coordinate getNextCoordinate() {
 
-        if(currentNumber > max) {
+        if ( currentNumber > max ) {
             currentNumber = min + 1;
-
-        } else {
+        }
+        else {
             currentNumber++;
         }
 
---
+++ src/copsandrobbers/randomgeneration/RandomCoordinateGenerator.java
@@ -0,0 +1,10 @@
+package copsandrobbers.randomgeneration;
+
+import copsandrobbers.Coordinate;
+
+/**
+ * Created by stephen on 10/27/15.
+ */
+public interface RandomCoordinateGenerator {
+    Coordinate getNextCoordinate();
+}
